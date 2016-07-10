faba89326469d55bb2689d88a3dece98f7b4e807
David Bjorklund <david.bjorklund@gmail.com>
2016-07-10 14:22:13 +0200

    don't install already installed packages

--- test/main-test.js
+++ test/main-test.js
@@ -111,3 +111,53 @@ test('simple package.json, production === true', function * (t) {
   ];
   t.deepEqual(actualDownloadArguments, expectedDownloadArguments);
 });
+
+test('already existing packages, one good & one needs updating', function * (t) {
+  const getIdealPackageTree = () => {
+    return Promise.resolve({
+      b: {
+        version: '2.5.9',
+        dependencies: {
+          c: {version: '3.0.1'}
+        }
+      },
+      c: {
+        version: '2.0.0'
+      }
+    });
+  };
+
+  const _actualDownloadArguments = [];
+  const downloadNpmPackage = ({arg, dir}) => function * (t) {
+    const [packageName] = arg.split('@');
+    _actualDownloadArguments.push({arg, dir});
+    yield mkdirp(join(dir, packageName));
+    yield fs.writeFile(join(dir, packageName, 'package.json'), '{}');
+  };
+
+  const {name: dir} = tmp();
+  yield mkdirp(join(dir, 'node_modules/b/node_modules/c'));
+  yield fs.writeFile(join(dir, 'package.json'), JSON.stringify({
+    devDependencies: {
+      b: '~2.5.1'
+    }
+  }));
+
+  yield fs.writeFile(join(dir, 'node_modules/b/package.json'), JSON.stringify({
+    name: 'b',
+    version: '2.5.9'
+  }));
+
+  yield fs.writeFile(join(dir, 'node_modules/b/node_modules/c/package.json'), JSON.stringify({
+    name: 'c',
+    version: '3.0.0'
+  }));
+
+  yield inject({getIdealPackageTree, downloadNpmPackage})({dir});
+  const actualDownloadArguments = sortBy(_actualDownloadArguments, 'arg');
+  const expectedDownloadArguments = [
+    {arg: 'c@2.0.0', dir: `${dir}/node_modules`},
+    {arg: 'c@3.0.1', dir: `${dir}/node_modules/b/node_modules`}
+  ];
+  t.deepEqual(actualDownloadArguments, expectedDownloadArguments);
+});
