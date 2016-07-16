ca7d83ea989ce589092368e7d7142defccf5a4a9
David Bjorklund <david.bjorklund@gmail.com>
2016-07-16 10:36:34 +0200

    add option to see code coverage, increase coverage

--- test/main-test.js
+++ test/main-test.js
@@ -112,3 +112,58 @@ test('simple package.json, production === true', async t => {
   ];
   t.deepEqual(actualDownloadArguments, expectedDownloadArguments);
 });
+
+test('dir defaults to cwd()', async t => {
+  const expectedArgs = [
+    'a@^1.0.0', 'b@~2.5.1'
+  ];
+
+  const getIdealPackageTree = actualArgs => {
+    t.deepEqual(actualArgs.sort(), expectedArgs);
+    return Promise.resolve({
+      a: {
+        version: '1.2.3'
+      },
+      b: {
+        version: '2.5.9',
+        dependencies: {
+          c: {version: '3.0.0'}
+        }
+      },
+      c: {
+        version: '2.0.0'
+      }
+    });
+  };
+
+  const _actualDownloadArguments = [];
+  const downloadPackage = async ({arg, dir}) => {
+    const [packageName] = arg.split('@');
+    _actualDownloadArguments.push({arg, dir});
+    await mkdirp(join(dir, packageName));
+    await fs.writeFile(join(dir, packageName, 'package.json'), '{}');
+  };
+
+  const {name: _dir} = tmp();
+  const dir = await fs.realpath(_dir);
+  await fs.writeFile(`${dir}/package.json`, JSON.stringify({
+    dependencies: {
+      a: '^1.0.0'
+    },
+    devDependencies: {
+      b: '~2.5.1'
+    }
+  }));
+
+  process.chdir(dir);
+
+  await inject({getIdealPackageTree, downloadPackage})({});
+  const actualDownloadArguments = sortBy(_actualDownloadArguments, 'arg');
+  const expectedDownloadArguments = [
+    {arg: 'a@1.2.3', dir: `${dir}/node_modules`},
+    {arg: 'b@2.5.9', dir: `${dir}/node_modules`},
+    {arg: 'c@2.0.0', dir: `${dir}/node_modules`},
+    {arg: 'c@3.0.0', dir: `${dir}/node_modules/b/node_modules`}
+  ];
+  t.deepEqual(actualDownloadArguments, expectedDownloadArguments);
+});
